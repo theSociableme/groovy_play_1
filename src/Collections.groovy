8cb6289d6521ce43b5bf3ea836131c45d5733d59
Mark Wagner <mark.wagner@reachlocal.com>
2015-02-19 12:18:27 -0600

    Learning string interpolation in groovy

--- src/Collections.groovy
+++ src/Collections.groovy
@@ -1,4 +1,5 @@
 def beatles = ["John", "Paul", "George", "Ringo"]
 for (int i = 0; i < beatles.size(); i++) {
-    println beatles[i]
+    def greeting = "Hello, "
+    println "$greeting" + beatles[i]
 }
