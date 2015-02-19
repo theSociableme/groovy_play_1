f535c42f0b53e73a9eed21d72ddf88077f7d3180
Mark Wagner <mark.wagner@reachlocal.com>
2015-02-19 12:51:24 -0600

    Groovy Dynamic typing

---
+++ src/Dynamic.groovy
@@ -0,0 +1,25 @@
+def cat = "meow"
+def one = 1
+
+println cat
+println cat.getClass()
+
+println one
+println one.getClass()
+
+//Cast Exception
+// Integer two = "two"
+
+// Call String Classes toUpperCase
+cat.toUpperCase()
+
+//one.toUpperCase()   // Creates Missing Method error
+
+one = "one"
+
+println one
+println one.getClass()
+
+
+
+
